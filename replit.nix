3dd6952a6ff01439b1a104223a484395d15b0c8f
Callum <callum@geekveggie.dev>
2025-11-10 17:57:09 +0000

    Post-Recovery checkpoint

--- replit.nix
+++
@@ -1,8 +0,0 @@
-{ pkgs }: {
-  deps = [
-    pkgs.nodePackages.vscode-langservers-extracted
-    pkgs.nodePackages.typescript-language-server  
-    pkgs.nodePackages_latest.npm
-    pkgs.nodejs-slim-19_x
-  ];
-}
